ccd7a4414b902c3ae60f042cc440110244f3bba7
ismail <fetayahismail@gmail.com>
2022-09-22 01:06:56 +0200

    almost finish

--- js/main.js
+++ js/main.js
@@ -3,20 +3,20 @@
 const sections = Array.from( document.getElementsByClassName('section'))
 const control =  Array.from(document.getElementsByClassName('control')) 
 const allSections = document.querySelector('.main-content')
-
+const themeBtn = document.querySelector('.theme-btn')
 control.forEach(element => {
     element.addEventListener('click',(e)=>{
         let currentBtn = document.querySelector('.active_btn')
         currentBtn.classList.remove('active_btn')
+        currentBtn.classList.remove('active')
         e.target.classList.add('active_btn')
-        
+        e.target.classList.add('active')
     })
 });
 
 allSections.addEventListener('click',(e)=>{
     let id = e.target.dataset.id
     let element = document.getElementById(id)
-    console.log(id)
     if(id){
         sections.forEach(sec=>{
             sec.classList.remove('active')
@@ -25,3 +25,7 @@ allSections.addEventListener('click',(e)=>{
     }
 })
 
+themeBtn.addEventListener('click',()=>{
+    let element = document.body
+    element.classList.toggle('light_mode')
+})
